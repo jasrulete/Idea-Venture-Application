b9bdfd6940a9211d1b2542a29907bc5cda4b2d0e
jasrulete <jsrulete@up.edu.ph>
2026-06-24 13:36:19 +0800

    fixed out of order information arrival for video

--- lib/webrtc.ts
+++ lib/webrtc.ts
@@ -107,7 +107,6 @@ export class PeerSession {
     this.ignoreOffer = !this.polite && offerCollision;
     if (this.ignoreOffer) return;
 
-    await this.flushPendingCandidates();
     await this.pc.setRemoteDescription(desc);
     if (desc.type === "offer") {
       await this.pc.setLocalDescription();
@@ -115,6 +114,7 @@ export class PeerSession {
         this.cb.onSignal("answer", JSON.stringify(this.pc.localDescription));
       }
     }
+    await this.flushPendingCandidates();
   }
 
   private async flushPendingCandidates() {
